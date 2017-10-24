31b64e4d611d2c8434631ef5e403a9858520e396
dwarson <dwarson@hotmail.com>
2017-10-24 00:22:42 -0400

    initial commit

---
+++ src/main/java/org/cccc/c4checkin/C4checkinApplication.java
@@ -0,0 +1,12 @@
+package org.cccc.c4checkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C4checkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C4checkinApplication.class, args);
+	}
+}
---
+++ src/test/java/org/cccc/c4checkin/C4checkinApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cccc.c4checkin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class C4checkinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
